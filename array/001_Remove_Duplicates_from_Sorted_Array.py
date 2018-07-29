e63a8c156499707b73cf23aaf726aab6d56d4d99
zhengyufan@wisers.com <yufan.zheng1@gmail.com>
2018-07-29 15:59:19 +0800

    tricky solution

--- array/001_Remove_Duplicates_from_Sorted_Array.py
+++ array/001_Remove_Duplicates_from_Sorted_Array.py
@@ -49,3 +49,96 @@ for (int i = 0; i < len; i++) {
 }
 
 """
+
+import unittest
+import random
+
+from utils.timer import timer
+
+
+class Solution(object):
+
+    @timer
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        i, pivot = 1, nums[0]
+        while i < len(nums):
+            if nums[i] == pivot:
+                nums.pop(i)
+            else:
+                pivot = nums[i]
+                i += 1
+        return i
+
+    @timer
+    def removeDuplicates_V2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums[:] = sorted(list(set(nums)))
+        return nums
+
+    def __repr__(self):
+        return 'Solution'
+
+
+class Test(unittest.TestCase):
+
+    # --------------------------------------------------------
+    # Normal cases
+    # --------------------------------------------------------
+
+    def test_1(self):
+        a = [1, 1, 2]
+        s = Solution()
+        l = s.removeDuplicates(a)
+        assert a == [1, 2]
+        assert l == 2
+
+    def test_2(self):
+        a = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+        s = Solution()
+        l = s.removeDuplicates(a)
+        assert a == [0, 1, 2, 3, 4]
+        assert l == 5
+
+    # --------------------------------------------------------
+    # Corner cases
+    # --------------------------------------------------------
+
+    def test_3(self):
+        a = [1, 1]
+        s = Solution()
+        l = s.removeDuplicates(a)
+        assert a == [1]
+        assert l == 1
+
+    def test_4(self):
+        a = [1]
+        s = Solution()
+        l = s.removeDuplicates(a)
+        assert a == [1]
+        assert l == 1
+
+    def test_5(self):
+        a = []
+        s = Solution()
+        l = s.removeDuplicates(a)
+        assert a == []
+        assert l == 0
+
+    def test_6(self):
+        a = [random.randrange(1, 10) for _ in range(0, 10000)]
+        s = Solution()
+        s.removeDuplicates(a)
+        s.removeDuplicates_V2(a)
+
+
+if __name__ == '__main__':
+    unittest.main()
